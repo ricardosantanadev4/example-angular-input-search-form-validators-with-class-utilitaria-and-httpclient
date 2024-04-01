654c22ab03face45556dc22767cf34c3af931ce0
ricardosantanadev <ricardosantanadev@gmail.com>
2024-04-01 16:04:54 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'example-angular-input-search-form-validators-with-class-utilitaria-and-httpclient';
+}
